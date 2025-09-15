3bf524130f697f0650d08e8017dca0d564e35a3d
Elizabethya <elizabethmleengomez@gmail.com>
2025-09-15 13:25:30 -0400

    Implementação do método venderProduto em ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProdutosDAO {
@@ -61,7 +62,29 @@ public class ProdutosDAO {
         }
         return listagem;
     }
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    conn = new conectaDAO().connectDB();
 
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+        }
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+    }
+    return listagem;
+}
+    
     public void venderProduto(int idProduto) {
         String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
         conn = new conectaDAO().connectDB();
